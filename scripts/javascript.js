b200b4a6299a122719c01c5f2df81d80b3044795
Pham Hoang Khoi <khoiphamkevin2004@gmail.com>
2022-11-01 16:13:06 -0700

    java and few addition

---
+++ scripts/javascript.js
@@ -0,0 +1,10 @@
+let a = 10;
+let b = 20;
+const x = "You" + "Are" + "Done";
+const y = true;
+if (console.log(typeof a) == console.log(typeof b)){
+    console.log("They match!")
+}
+else {
+    console.log("No Luck!")
+}
